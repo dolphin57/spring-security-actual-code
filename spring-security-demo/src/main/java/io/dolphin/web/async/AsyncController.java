efffa9c79a27fa6b74bcf38f925ff8e274a72976
dolphin <ericql57@163.com>
2021-04-05 16:03:03 +0800

    feat:callable实现线程池

---
+++ spring-security-demo/src/main/java/io/dolphin/web/async/AsyncController.java
@@ -0,0 +1,41 @@
+package io.dolphin.web.async;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @author Eric
+ * @create 2019 07 21 8:41
+ */
+@RestController
+@Slf4j
+public class AsyncController {
+    //@Autowired
+    //private MockQueue mockQueue;
+    //
+    //@Autowired
+    //private DeferredResultHolder deferredResultHolder;
+
+    @RequestMapping("/order")
+    public Callable<String> order() throws InterruptedException {
+        log.info("主线程开始");
+        Callable<String> result = new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                log.info("副线程开始");
+                Thread.sleep(1000);
+                log.info("副线程返回");
+                return "success";
+            }
+        };
+
+        log.info("主线程返回");
+        return result;
+    }
+}
