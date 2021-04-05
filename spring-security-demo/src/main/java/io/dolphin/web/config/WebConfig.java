eed667972d656b8dae3efe939ddba28ffa24ea7a
dolphin <ericql57@163.com>
2021-04-05 16:19:42 +0800

    feat:异步处理配置

--- spring-security-demo/src/main/java/io/dolphin/web/config/WebConfig.java
+++ spring-security-demo/src/main/java/io/dolphin/web/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -38,4 +39,13 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(timeInterceptor);
     }
+
+    //@Override
+    //public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+    //    super.configureAsyncSupport(configurer);
+    //    configurer.registerCallableInterceptors();
+    //    configurer.registerDeferredResultInterceptors();
+    //    configurer.setDefaultTimeout();
+    //    configurer.setTaskExecutor();
+    //}
 }
