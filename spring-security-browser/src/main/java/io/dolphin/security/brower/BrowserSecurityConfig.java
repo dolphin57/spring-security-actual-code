e797e09cfb5aef843f2cf1fae6187f0a368995be
dolphin <ericql57@163.com>
2021-04-06 08:07:11 +0800

    feat:自定义用户认证逻辑

--- spring-security-browser/src/main/java/io/dolphin/security/brower/BrowserSecurityConfig.java
+++ spring-security-browser/src/main/java/io/dolphin/security/brower/BrowserSecurityConfig.java
@@ -1,8 +1,11 @@
 package io.dolphin.security.brower;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author Eric
@@ -10,6 +13,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
---
+++ spring-security-browser/src/main/java/io/dolphin/security/brower/MyUserDetailsService.java
@@ -0,0 +1,33 @@
+package io.dolphin.security.brower;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Eric
+ * @create 2019 07 22 21:58
+ */
+@Component
+@Slf4j
+public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("登陆用户名:" + username);
+        // 根据用户名查找用户信息
+        // 根据查找到的用户信息判断用户是否被冻结
+        String password = passwordEncoder.encode("123456");
+        log.info("数据库密码:"+password);
+        return new User(username, password,
+                AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+    }
+}
